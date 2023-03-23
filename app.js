fb0766ad39cef0aaf68bc69bb841daa5fd748a9e
sal <shaurya.kharb@sumanastech.com>
2023-03-23 23:47:03 +0530

    Creates a jira issue

--- app.js
+++ app.js
@@ -1,62 +1,168 @@
-const { App } =  require("@slack/bolt");
+const { App } = require("@slack/bolt");
+const https = require("node:https");
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    appToken: process.env.SLACK_APP_TOKEN,
-    port: process.env.PORT || 3000,
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000,
 });
 
-app.shortcut('open_modal', async ({ shortcut, ack, client, logger }) => {
-    try {
+app.view("create_view", async ({ ack, body, view, client, logger }) => {
+  try {
+    await ack();
 
-        await ack();
+    const user = body["user"]["id"];
 
-        const result = await client.views.open({
-            trigger_id: shortcut.trigger_id,
-            view: {
-                type: "modal",
-                title: {
-                    type: "plain_text",
-                    text: "My App"
-                },
-                close: {
-                    type: "plain_text",
-                    text: "Close"
-                },
-                blocks: [
-                    {
-                        type: "section",
-                        text: {
-                            type: "mrkdwn",
-                            text: "About the simplest modal you could conceive of :smile:"
-                        }
-                    },
-                    {
-                        type: "context",
-                        elements: [
-                            {
-                                type: "mrkdwn",
-                                text: "Psssst this modal was designed using"
-                            }
-                        ]
-                    }
-                ]
-            }
-        });
+    const title = view["state"]["values"]["block_1"]["title_input"].value;
+    const summ = view["state"]["values"]["block_2"]["summ_input"].value;
 
-        logger.info(result);
+    console.log(title, summ);
 
-    } catch(e){
-        logger.error(error);
-    }
+    createJiraTicket(title, summ);
+
+    await client.chat.postMessage({
+      channel: user,
+      text: "Ticket Created",
+    });
+  } catch (e) {
+    logger.error(e);
+  }
 });
+
+app.shortcut("create", async ({ shortcut, ack, client, logger }) => {
+  console.log("open");
+  try {
+    await ack();
+
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: {
+        callback_id: "create_view",
+        title: {
+          type: "plain_text",
+          text: "Create Issue",
+        },
+        submit: {
+          type: "plain_text",
+          text: "Create",
+        },
+        blocks: [
+          {
+            type: "input",
+            block_id: "block_1",
+            element: {
+              type: "plain_text_input",
+              action_id: "title_input",
+              placeholder: {
+                type: "plain_text",
+                text: "Your Response",
+              },
+            },
+            label: {
+              type: "plain_text",
+              text: "Title",
+            },
+          },
+          {
+            type: "input",
+            block_id: "block_2",
+            element: {
+              type: "plain_text_input",
+              action_id: "summ_input",
+              multiline: true,
+              placeholder: {
+                type: "plain_text",
+                text: "Your Response",
+              },
+            },
+            label: {
+              type: "plain_text",
+              text: "Summary",
+            },
+          },
+        ],
+        type: "modal",
+      },
+    });
+
+    logger.info(result);
+  } catch (e) {
+    logger.error(e);
+  }
+});
+
+function createJiraTicket(title, summ) {
+  const postData = JSON.stringify({
+    fields: {
+      description: {
+        content: [
+          {
+            content: [
+              {
+                text: summ,
+                type: "text",
+              },
+            ],
+            type: "paragraph",
+          },
+        ],
+        type: "doc",
+        version: 1,
+      },
+      issuetype: {
+        id: "10001",
+      },
+      labels: ["bugfix", "blitz_test"],
+      project: {
+        id: "10000",
+      },
+      summary: title,
+    },
+    update: {},
+  });
+
+  let auth = process.env.JIRA_EMAIL + ":" + process.env.JIRA_TOKEN;
+  auth = Buffer.from(auth).toString("base64");
+
+  const options = {
+    hostname: process.env.JIRA_HOST,
+    port: 443,
+    path: "/rest/api/3/issue",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": Buffer.byteLength(postData),
+      "Authorization": "Basic " + auth,
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding("utf8");
+    res.on("data", (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on("end", () => {
+      console.log("No more data in response.");
+    });
+  });
+
+  req.on("error", (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // Write data to request body
+  req.write(postData);
+  req.end();
+}
 
 (async () => {
-    await app.start();
-    console.log('Listening...');
+  await app.start();
+  console.log("Listening...");
 })();
